9201013c6473aaef0a54ed8592e8062e33543e25
Zak Strassberg <zakstrassberg@gmail.com>
2021-12-30 13:31:41 -0800

    slug page

--- app/routes/charms/$slug[.ttl].ts
+++ app/routes/charms/$slug[.ttl].ts
@@ -3,10 +3,13 @@ import Charm from "../../../services/Charm";
 import N3Service from "../../../services/n3";
 
 export const loader: LoaderFunction = async ({ request, params }) => {
-  const charms = await Charm.load();
-  const charm = charms.filter((c) => c.subject.value === params.slug);
+  const charms = N3Service.createStore(await Charm.load());
+  let charmQuads = charms.getQuads(request.url, null, null, null);
+  if (charmQuads.length === 0) {
+    charmQuads = charms.getQuads(params.slug as string, null, null, null);
+  }
   try {
-    const res = await N3Service.quadsToTurtle(charm);
+    const res = await N3Service.quadsToTurtle(charmQuads);
     return new Response(res, { headers: { "Content-Type": "text/turtle" } });
   } catch (ex) {
     console.error(ex)
